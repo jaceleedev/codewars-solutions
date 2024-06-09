5e0359485856f19cafc3e9db66a2af2381402b5d
jaceleedev <jaceleedev@gmail.com>
2024-06-10 08:41:05 +0900

    feat: :technologist: solve 8kyu Are You Playing Banjo?

---
+++ 8kyu/Are You Playing Banjo?.js
@@ -0,0 +1,7 @@
+// Are You Playing Banjo?
+function areYouPlayingBanjo(name) {
+  // Implement me
+  return name[0].toLowerCase() === 'r'
+    ? `${name} plays banjo`
+    : `${name} does not play banjo`;
+}
