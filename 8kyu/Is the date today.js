168b3bcb9bcdeb7d1ddfc14f093904fb60e5df02
jaceleedev <jaceleedev@gmail.com>
2024-06-29 21:20:58 +0900

    feat: :technologist: solve 8kyu Is the date today

---
+++ 8kyu/Is the date today.js
@@ -0,0 +1,11 @@
+// Is the date today
+function isToday(date) {
+  //Code goes here.
+  const today = new Date();
+
+  return (
+    date.getFullYear() === today.getFullYear() &&
+    date.getMonth() === today.getMonth() &&
+    date.getDate() === today.getDate()
+  );
+}
