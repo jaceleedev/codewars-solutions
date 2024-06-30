01c8d5d8f109c2ec04302c4a13fc5220d708c110
jaceleedev <jaceleedev@gmail.com>
2024-06-30 20:02:50 +0900

    feat: :technologist: solve 8kyu Up and down, the string grows

---
+++ 8kyu/Up and down, the string grows.js
@@ -0,0 +1,5 @@
+// Up and down, the string grows
+// Note: As of June 30, 2024, the problem cannot be solved using JavaScript due to size constraints.
+// The solution is implemented in Ruby as follows:
+// STRANGE_STRING = 'ß'
+const STRANGE_STRING = 'ß';
