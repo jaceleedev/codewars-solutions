9fd85afd1be3dcea0a797d9121922102c32e79e7
jaceleedev <jaceleedev@gmail.com>
2024-07-08 10:34:34 +0900

    feat: :technologist: solve 8kyu 101 Dalmatians - squash the bugs, not the dogs!

---
+++ 8kyu/101 Dalmatians - squash the bugs, not the dogs!.js
@@ -0,0 +1,20 @@
+// 101 Dalmatians - squash the bugs, not the dogs!
+function howManyDalmatians(number) {
+  var dogs = [
+    'Hardly any',
+    'More than a handful!',
+    "Woah that's a lot of dogs!",
+    '101 DALMATIANS!!!',
+  ];
+
+  var respond =
+    number <= 10
+      ? dogs[0]
+      : number <= 50
+      ? dogs[1]
+      : number === 101
+      ? dogs[3]
+      : dogs[2];
+
+  return respond;
+}
