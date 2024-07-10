21c7b5f04846898ba72fa45849b3db2540a09e3d
jaceleedev <jaceleedev@gmail.com>
2024-07-10 20:42:45 +0900

    feat: :technologist: solve 8kyu L1: Bartender, drinks!

---
+++ 8kyu/L1: Bartender, drinks!.js
@@ -0,0 +1,21 @@
+// L1: Bartender, drinks!
+function getDrinkByProfession(param) {
+  const input = param.toLowerCase();
+
+  switch (input) {
+    case 'jabroni':
+      return 'Patron Tequila';
+    case 'school counselor':
+      return 'Anything with Alcohol';
+    case 'programmer':
+      return 'Hipster Craft Beer';
+    case 'bike gang member':
+      return 'Moonshine';
+    case 'politician':
+      return 'Your tax dollars';
+    case 'rapper':
+      return 'Cristal';
+    default:
+      return 'Beer';
+  }
+}
