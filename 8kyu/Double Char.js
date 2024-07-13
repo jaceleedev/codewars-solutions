8908516daa89ba5d53811f2b37294c716368b874
jaceleedev <jaceleedev@gmail.com>
2024-07-13 21:57:44 +0900

    feat: :technologist: solve 8kyu Double Char

---
+++ 8kyu/Double Char.js
@@ -0,0 +1,11 @@
+// Double Char
+function doubleChar(str) {
+  // Your code here
+  let result = '';
+
+  for (let i = 0; i < str.length; ++i) {
+    result += `${str[i]}${str[i]}`;
+  }
+
+  return result;
+}
