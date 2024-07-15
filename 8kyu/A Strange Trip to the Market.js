73f58cb2b7376ec2a199a90c835d314869376ed6
jaceleedev <jaceleedev@gmail.com>
2024-07-15 15:48:40 +0900

    feat: :technologist: solve 8kyu A Strange Trip to the Market

---
+++ 8kyu/A Strange Trip to the Market.js
@@ -0,0 +1,7 @@
+// A Strange Trip to the Market
+function isLockNessMonster(s) {
+  //FIND THE LOCH NESS MONSTER. SAVE YOUR TREE FIDDY
+  return (
+    s.includes('tree fiddy') || s.includes('3.50') || s.includes('three fifty')
+  );
+}
