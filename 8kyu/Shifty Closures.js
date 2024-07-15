d053d259b4233aa220d68ea61871f0880fa8ba4c
jaceleedev <jaceleedev@gmail.com>
2024-07-15 15:38:24 +0900

    feat: :technologist: solve 8kyu Shifty Closures

---
+++ 8kyu/Shifty Closures.js
@@ -0,0 +1,12 @@
+// Shifty Closures
+const greetAbe = () => {
+  const name = 'Abe';
+
+  return 'Hello, ' + name + '!';
+};
+
+const greetBen = () => {
+  const name = 'Ben';
+
+  return 'Hello, ' + name + '!';
+};
