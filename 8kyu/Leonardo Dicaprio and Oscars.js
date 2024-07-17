9663c1c043bc54e3923c9514691acc91c2c80321
jaceleedev <jaceleedev@gmail.com>
2024-07-17 10:27:28 +0900

    feat: :technologist: solve 8kyu Leonardo Dicaprio and Oscars

---
+++ 8kyu/Leonardo Dicaprio and Oscars.js
@@ -0,0 +1,18 @@
+// Leonardo Dicaprio and Oscars
+function leo(oscar) {
+  if (oscar === 86) {
+    return 'Not even for Wolf of wallstreet?!';
+  }
+
+  if (oscar === 88) {
+    return 'Leo finally won the oscar! Leo is happy';
+  }
+
+  if (oscar < 88) {
+    return 'When will you give Leo an Oscar?';
+  }
+
+  if (oscar > 88) {
+    return 'Leo got one already!';
+  }
+}
