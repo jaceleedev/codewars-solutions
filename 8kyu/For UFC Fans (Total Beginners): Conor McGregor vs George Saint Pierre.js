e4fb43798f4d64336b476aa2ff4a4ed8adaf0992
jaceleedev <jaceleedev@gmail.com>
2024-07-27 17:40:43 +0900

    feat: :technologist: solve 8kyu For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre

---
+++ 8kyu/For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.js
@@ -0,0 +1,9 @@
+// For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+var quote = function (fighter) {
+  // your code here
+  if (fighter.toLowerCase() === 'george saint pierre') {
+    return 'I am not impressed by your performance.';
+  }
+
+  return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+};
