3c5e822742652441d1f7ee68e4400f795c5286e5
jaceleedev <jaceleedev@gmail.com>
2024-08-06 15:55:58 +0900

    feat: :technologist: solve 8kyu For Twins: 2. Math operations

---
+++ 8kyu/For Twins: 2. Math operations.js
@@ -0,0 +1,7 @@
+// For Twins: 2. Math operations
+function iceBrickVolume(radius, bottleLength, rimLength) {
+  // Your code should be here ;)
+  const height = bottleLength - rimLength;
+
+  return radius * radius * height * 2;
+}
