bc889d5d2641037646e3e0cb383c5d88ed904252
jaceleedev <jaceleedev@gmail.com>
2024-08-11 20:02:57 +0900

    feat: :technologist: solve 8kyu Sum of differences in array

---
+++ 8kyu/Sum of differences in array.js
@@ -0,0 +1,11 @@
+// Sum of differences in array
+function sumOfDifferences(arr) {
+  arr.sort((a, b) => b - a);
+  let sum = 0;
+
+  for (let i = 0; i < arr.length - 1; ++i) {
+    sum += arr[i] - arr[i + 1];
+  }
+
+  return sum;
+}
