e6d7ce3e171b314f6d3b0260ef1f6069342dbbc8
jaceleedev <jaceleedev@gmail.com>
2024-08-23 13:29:02 +0900

    feat: :technologist: solve 8kyu Grader

---
+++ 8kyu/Grader.js
@@ -0,0 +1,16 @@
+// Grader
+function grader(score) {
+  if (score > 1 || score < 0.6) {
+    return 'F';
+  }
+
+  if (score >= 0.9) {
+    return 'A';
+  } else if (score >= 0.8) {
+    return 'B';
+  } else if (score >= 0.7) {
+    return 'C';
+  } else if (score >= 0.6) {
+    return 'D';
+  }
+}
