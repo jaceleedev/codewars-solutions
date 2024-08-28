68cdb4983844b276fa7dc0bb1125b2832e359fb4
jaceleedev <jaceleedev@gmail.com>
2024-08-28 13:24:39 +0900

    feat: :technologist: solve 8kyu Get Planet Name By ID

---
+++ 8kyu/Get Planet Name By ID.js
@@ -0,0 +1,32 @@
+// Get Planet Name By ID
+function getPlanetName(id) {
+  let name;
+
+  switch (id) {
+    case 1:
+      name = 'Mercury';
+      break;
+    case 2:
+      name = 'Venus';
+      break;
+    case 3:
+      name = 'Earth';
+      break;
+    case 4:
+      name = 'Mars';
+      break;
+    case 5:
+      name = 'Jupiter';
+      break;
+    case 6:
+      name = 'Saturn';
+      break;
+    case 7:
+      name = 'Uranus';
+      break;
+    case 8:
+      name = 'Neptune';
+  }
+
+  return name;
+}
