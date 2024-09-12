5abfbc482a68270db2e08164cb726af581d390af
jaceleedev <jaceleedev@gmail.com>
2024-09-12 14:08:20 +0900

    feat: :technologist: solve 8kyu Drink about

---
+++ 8kyu/Drink about.js
@@ -0,0 +1,12 @@
+// Drink about
+function peopleWithAgeDrink(old) {
+  if (old >= 21) {
+    return 'drink whisky';
+  } else if (old >= 18) {
+    return 'drink beer';
+  } else if (old >= 14) {
+    return 'drink coke';
+  } else {
+    return 'drink toddy';
+  }
+}
